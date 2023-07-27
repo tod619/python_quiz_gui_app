a12fec2b9a6abf37312a08f9f188a94e595268a9
tod619 <trevorodwyer619@hotmail.com>
2023-07-27 22:18:58 +0100

    Add label for score & canvas for question txt in ui.py file

--- ui.py
+++ ui.py
@@ -9,4 +9,16 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizz App")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     text="some question text",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
         self.window.mainloop()
