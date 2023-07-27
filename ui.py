900540f2271668b372e392f6ab7806520c529e54
tod619 <trevorodwyer619@hotmail.com>
2023-07-27 22:25:12 +0100

    Create true & false buttons in ui.py file

--- ui.py
+++ ui.py
@@ -21,4 +21,12 @@ class QuizInterface:
                                                      font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
         self.window.mainloop()
