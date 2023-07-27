92a7529e96e357c5f20bf8924f0859aa0d08cf09
tod619 <trevorodwyer619@hotmail.com>
2023-07-27 22:05:49 +0100

    add padding + bg color to window in ui.py file

--- ui.py
+++ ui.py
@@ -8,5 +8,5 @@ class QuizInterface:
     def __init__(self) -> None:
         self.window = Tk()
         self.window.title("Quizz App")
-
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.window.mainloop()
