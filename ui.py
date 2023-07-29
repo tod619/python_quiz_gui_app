a1cd3c38664a085e2c32bd9ad2fe643fdfd52a23
tod619 <trevorodwyer619@hotmail.com>
2023-07-29 21:36:26 +0100

    Get Question text to wrap in GUI in ui.py file

--- ui.py
+++ ui.py
@@ -20,6 +20,7 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(150,
                                                      125,
+                                                     width=280,
                                                      text="some question text",
                                                      fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
